3d868558f57cc8341a4775aa3b7748955c19325a
Priscilha <priscilha@gmail.com>
2023-06-29 10:51:36 -0300

    iniciando js

---
+++ script.js
@@ -0,0 +1,32 @@
+//variáveis
+const form = document.querySelector('form')
+const inputWeight = document.querySelector('#weight')
+const inputHeight = document.querySelector('#height')
+
+const modalWrapper = document.querySelector('.modal-wrapper')
+const modalMessage = document.querySelector('.modal .title span')
+const modalBtnClose = document.querySelector('.modal button.close')
+
+
+
+
+form.onsubmit = function(event) {
+    event.preventDefault()
+
+    const weight = inputWeight.value
+    const height = inputHeight.value
+
+    const result = IMC(weight, height)
+    const message = `Seu IMC é de ${result}`
+
+    modalMessage.innerText = message
+    modalWrapper.classList.add('open')
+}
+
+modalBtnClose.onclick = function () {
+    modalWrapper.classList.remove('open')
+}
+
+function IMC (weight, height) {
+    return (weight / ((height / 100)**2)).toFixed(2)
+}
