135d5bb5a740ca61bb03f1413cf1bf07f6bc5ce1
Priscilha <priscilha@gmail.com>
2023-09-08 19:28:37 -0300

    Modal

--- script.js
+++ script.js
@@ -3,13 +3,18 @@ const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
 const inputHeight = document.querySelector('#height')
 
-const modalWrapper = document.querySelector('.modal-wrapper')
-const modalMessage = document.querySelector('.modal .title span')
-const modalBtnClose = document.querySelector('.modal button.close')
+
+
 
 const Modal = {
-    open () {modalWrapper.classList.add('open')},
-    close () {modalWrapper.classList.remove('open')}
+
+    wrapper: document.querySelector('.modal-wrapper'),
+    message: document.querySelector('.modal .title span'),
+    buttonClose: document.querySelector('.modal button.close'),
+
+
+    open () {Modal.wrapper.classList.add('open')},
+    close () {Modal.wrapper.classList.remove('open')}
 }
 
 
@@ -25,11 +30,11 @@ form.onsubmit = function(event) {
     const result = IMC(weight, height)
     const message = `Seu IMC é de ${result}`
 
-    modalMessage.innerText = message
+    Modal.message.innerText = message
     Modal.open()
 }
 
-modalBtnClose.onclick = () => {
+Modal.buttonClose.onclick = () => {
     Modal.close()
 }
 
